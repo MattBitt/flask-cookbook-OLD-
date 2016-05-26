95f6276efb75536ee16afd30cb362a6851d738bd
MattBitt <mattsbitt@gmail.com>
2016-05-26 10:57:22 -0500

    more scrapers with fixtures working

--- tests/test_scrapers.py
+++ tests/test_scrapers.py
@@ -1,32 +1,73 @@
 import unittest
-from utils.scraper import web_scraper
+import utils.scraper
+import os
 
 
 
 class TestScrapers(unittest.TestCase):
+    def setUp(self):
+        # mock the download function to use local files
+        utils.scraper.urllib.urlopen = dummy_download
+        
+    
     def test_seriouseats(self):
         url = "http://www.seriouseats.com/recipes/2016/05/spicy-spring-sicilian-pizza-recipe.html"
-        sereats = web_scraper(url)
-        #print sereats.title
+        sereats = utils.scraper.web_scraper(url)
         assert sereats.title == r"Sicilian Pizza With Pepperoni and Spicy Tomato Sauce Recipe"
+        assert len(sereats.ingredients) == 18
         assert "Kosher salt" in sereats.ingredients
-        #for d in sereats.directions:
-        #    print d.strip() + "\n"
-        assert "Transfer dough to baking sheet" in sereats.directions
+        assert len(sereats.directions) == 8
+        assert "Transfer dough to baking sheet" in sereats.directions[3]
+        assert sereats.url == url
+        assert sereats.image_url == 'http://www.seriouseats.com/recipes/assets_c/2016/05/20160503-spicy-spring-pizza-recipe-37-thumb-1500xauto-431711.jpg'
         
     def test_skinnytaste(self):
         url = "http://www.skinnytaste.com/gochujang-glazed-salmon/"
-        skta = web_scraper(url)
-        #print skta.title
-        assert skta.title == r"SkinnyTaste Title"
-        #assert "Kosher salt" in sereats.ingredients
-        #for d in sereats.directions:
-        #    print d.strip() + "\n"
-        #assert "Transfer dough to baking sheet" in sereats.directions
+        skta = utils.scraper.web_scraper(url)
+        assert skta.title == r"Gochujang-Glazed Salmon"
+        assert skta.url == url
+        assert skta.total_time == "PT10M"
+        assert len(skta.ingredients) == 11
+        assert '2 tsp mirin' in skta.ingredients
+        assert 'the broiler pan with foil' in skta.directions[1]
+        assert skta.image_url == 'http://www.skinnytaste.com/wp-content/uploads/2016/05/Gochujang-Glazed-Salmon-4-550x825.jpg'
         
-    def test_other(self):
-        url = "http://www.foodnetwork.com/recipes/marcela-valladolid/baja-style-fish-tacos-recipe.html"
-        other = web_scraper(url)
-        print other.title
+    def test_foodnetwork(self):
+        url = "http://www.foodnetwork.com/recipes/alton-brown/baked-macaroni-and-cheese-recipe.html"
+        foodn = utils.scraper.web_scraper(url)
+        assert foodn.title == 'Baked Macaroni and Cheese'
+        assert foodn.url == url
+        assert foodn.total_time == 'PT1H5M'
+        assert len(foodn.ingredients) == 14
+        assert '3 tablespoons butter' in foodn.ingredients
+        assert 'While the pasta' in foodn.directions[2]
+        assert foodn.image_url == 'http://foodnetwork.sndimg.com/content/dam/images/food/fullset/2011/6/6/0/EA1E10_Baked-Macaraoni-and-Cheese_s4x3.jpg.rend.sniipadlarge.jpeg'
+        
+    def test_hrecipe(self):
+        url = "hrecipe"
+        hrecipe = utils.scraper.web_scraper(url)
+        assert hrecipe.title == r"Sicilian Pizza With Pepperoni and Spicy Tomato Sauce Recipe"
+        assert len(hrecipe.url) > 0
 
+    def test_not_hrecipe(self):
+        ########################### not working #######################################
+        url = "not_hrecipe"
+        not_hrecipe = utils.scraper.web_scraper(url)
         
+
+def dummy_download(url):
+    fixtures = {'seriouseats.com' : 'serious_eats.html',
+                'skinnytaste.com' : 'skinnytaste.html',
+                'foodnetwork.com' : 'foodnetwork.html',
+                'hrecipe' : 'hrecipe.html',
+                'not_hrecipe': 'not_hrecipe.html'}
+    
+    fname = 'hrecipe.html'
+    for k,v in fixtures.iteritems():
+        if k in url:
+            fname = v
+    
+    f = open(os.path.join('cookbook', 'fixtures', fname), 'r')
+    html = f.read()
+    f.close()
+    return html
--- utils/scraper.py
+++ utils/scraper.py
@@ -6,12 +6,21 @@ def web_scraper(url):
     """ Class factory function. Returns a scraper object based on the url """
     """ should first check if an hRecipe compliant site """
     html = download_url(url)
+    
     if 'seriouseats' in url:
         return SeriousEatsScraper(html)
     elif 'skinnytaste' in url:
         return SkinnyTasteScraper(html)
+    elif 'foodnetwork' in url:
+        return FoodNetworkScraper(html)
     else:
-        return GenericScraper(html)
+        if 'hrecipe' in html:
+            return GenericHRecipeScraper(html)
+        else:
+            ####################### never reaching this point ################
+            print "Should see error"
+            # should be a custom error that I catch above
+            raise NameError
 
 def download_url(url):
     return urllib.urlopen(url)
@@ -24,36 +33,90 @@ class Scraper(object):
     
     @property
     def title(self):
+        return self.soup.find('meta', {'property' : 'og:title'}).get('content')
+    
+    @property
+    def ingredients(self):
         raise NotImplementedError
+    
+    @property
+    def directions(self):
+        raise NotImplementedError
+    
+    @property
+    def url(self):
+        return self.soup.find('meta', {'property' : 'og:url'}).get('content')
 
-class SeriousEatsScraper(Scraper):
+    def get_list_contents(self, tag, class_name):
+        list_items_with_tags = self.soup.find_all(tag, {'class' : class_name})
+        list_items = []
+        for l in list_items_with_tags:
+            list_items.append(l.text)
+        return list_items
 
     @property
-    def title(self):
-        return self.soup.find('h1', {'class' : 'recipe-title'}).text
-
+    def image_url(self):
+        return self.soup.find('meta', {'property' : 'og:image'}).get('content') 
+        
+class SeriousEatsScraper(Scraper):
     @property 
     def ingredients(self):
-        return self.get_list_contents('ingredient')
+        return self.get_list_contents('li', 'ingredient')
 
     @property
     def directions(self):
-        return self.get_list_contents('recipe-procedure-text')
+        return self.get_list_contents('div', 'recipe-procedure-text')
+
         
+class SkinnyTasteScraper(Scraper):
+    @property
+    def total_time(self):
+        return self.soup.find('meta', {'itemprop' : 'totalTime'}).get('content')
+        
+    @property
+    def ingredients(self):
+        list_items_with_tags = self.soup.find_all('li', {'itemprop' : 'ingredients'})
+        list_items = []
+        for l in list_items_with_tags:
+            list_items.append(l.text)
+        return list_items  
+        
+    @property
+    def directions(self):
+        list_items_with_tags = self.soup.find('span', {'itemprop' : 'recipeInstructions'}).find('ol').find_all('li')
+        list_items = []
+        for l in list_items_with_tags:
+            list_items.append(l.text)
+        return list_items 
+
+
+class GenericHRecipeScraper(Scraper):
+    @property
+    def directions(self):
+        return "Generic Directions"
+        
+        
+class FoodNetworkScraper(Scraper):
+    @property
+    def title(self):
+        return self.soup.find('div', {'class' : 'title'}).find('h1').text
     
-    def get_list_contents(self, class_name):
-        list_items_with_tags = self.soup.find_all('div', {'class' : class_name})
+    @property
+    def total_time(self):
+        return self.soup.find('meta', {'itemprop' : 'totalTime'}).get('content')
+        
+    @property
+    def ingredients(self):
+        list_items_with_tags = self.soup.find_all('li', {'itemprop' : 'ingredients'})
         list_items = []
         for l in list_items_with_tags:
             list_items.append(l.text)
         return list_items
         
-class SkinnyTasteScraper(Scraper):
-    @property
-    def title(self):
-        return "SkinnyTaste Title"
-
-class GenericScraper(Scraper):
     @property
     def directions(self):
-        return "Generic Directions"
+        list_items_with_tags = self.soup.find('ul', {'class' : 'recipe-directions-list'}).find_all('li')
+        list_items = []
+        for l in list_items_with_tags:
+            list_items.append(l.find('p').text)
+        return list_items
